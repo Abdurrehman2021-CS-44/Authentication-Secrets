d0b0be1181ffa1767ab448f90ba7ec4a76ee10bf
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-07-08 15:44:15 +0500

    Setup mongodb database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -9,6 +10,21 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+.then(()=>{
+    console.log("Database Connected.");
+})
+.catch((err)=>{
+    console.log(err);
+});
+
+const userSchema = {
+    username: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res)=>{
     res.render("home");
